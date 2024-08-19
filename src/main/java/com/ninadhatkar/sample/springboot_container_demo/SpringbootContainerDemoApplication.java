d028fabe6d113e29a9bc930c3b58a961a8c55a47
Ninad Hatkar <ninadhatkar@airasia.com>
2024-08-19 23:38:06 +0530

    Your commit message

---
+++ src/main/java/com/ninadhatkar/sample/springboot_container_demo/SpringbootContainerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ninadhatkar.sample.springboot_container_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootContainerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootContainerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ninadhatkar/sample/springboot_container_demo/SpringbootContainerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninadhatkar.sample.springboot_container_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootContainerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
